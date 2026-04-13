e2cdf26e2164709f3e5262631d2bdc46a5a85976
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-14 00:23:50 +0530

    :zap: :package: :art: added scss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  /* SCSS configuaration */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import variables from "./styles/variables.module.scss";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className={variables.title}>
+        Hello Product 
+        <Link href="/contact">Contact</Link>
+      </h1>
+    </div>
+  );
 }
